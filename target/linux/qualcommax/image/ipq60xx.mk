83dc60c228f7861f3c7e585ddee3f64501ce5788
1Conan <me@1conan.com>
2024-04-10 16:01:17 +0800

    qualcommax: cleanup mr7350 image config

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -1,17 +1,3 @@
-define Device/linksys_mr7350
-     $(call Device/FitImage)
-     DEVICE_VENDOR := Linksys
-     DEVICE_MODEL := MR7350
-     BLOCKSIZE := 128k
-     PAGESIZE := 2048
-     SOC := ipq6018
-     KERNEL_SIZE := 8192k
-     IMAGES += factory.bin
-     IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | linksys-image type=MR7350
-     DEVICE_PACKAGES := ipq-wifi-linksys_mr7350 kmod-leds-pca963x
-endef
-TARGET_DEVICES += linksys_mr7350
-
 define Device/8devices_mango-dvk
 	$(call Device/FitImageLzma)
 	DEVICE_VENDOR := 8devices
@@ -24,6 +10,21 @@ define Device/8devices_mango-dvk
 	DEVICE_PACKAGES := ipq-wifi-8devices_mango
 endef
 TARGET_DEVICES += 8devices_mango-dvk
+
+define Device/linksys_mr7350
+	$(call Device/FitImage)
+	DEVICE_VENDOR := Linksys
+	DEVICE_MODEL := MR7350
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+       KERNEL_SIZE := 8192k
+	SOC := ipq6018
+	IMAGES += factory.bin
+	IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | \
+		append-ubi | linksys-image type=MR7350
+	DEVICE_PACKAGES := ipq-wifi-linksys_mr7350 kmod-leds-pca963x
+endef
+TARGET_DEVICES += linksys_mr7350
 
 define Device/netgear_wax214
        $(call Device/FitImage)
