9703e314d98dec68ee68aa9ee88936e06ed491b9
1Conan <me@1conan.com>
2024-05-11 23:59:15 +0800

    qualcommax: ipq60xx: set CPU type to cortex-a53
    Set the target arch to Cortex-A73. Packages are still set as A53 to use the their feeds. Locally built packages will use the A73 toolchain and optimizations.
    
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- target/linux/qualcommax/ipq60xx/target.mk
+++ target/linux/qualcommax/ipq60xx/target.mk
@@ -1,6 +1,8 @@
 SUBTARGET:=ipq60xx
 FEATURES += source-only
 BOARDNAME:=Qualcomm Atheros IPQ60xx
+CPU_TYPE:=cortex-a73
+ARCH_PACKAGES:=aarch64_cortex-a53
 DEFAULT_PACKAGES += ath11k-firmware-ipq6018
 
 define Target/Description
